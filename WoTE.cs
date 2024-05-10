8eb5c5e11859a7574231dcd4d8bf420bbb233a82
Dominic Karma <farfetchedking541@gmail.com>
2024-05-10 02:58:03 -0500

    Thank you TML, I didn't know

--- WoTE.cs
+++ WoTE.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 
 namespace WoTE
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class WoTE : Mod
-	{
+    public class WoTE : Mod
+    {
 
-	}
+    }
 }
