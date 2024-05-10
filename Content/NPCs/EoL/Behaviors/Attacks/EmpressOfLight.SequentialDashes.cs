f84bddc139be891ce6941917881104a89753ff56
Dominic Karma <farfetchedking541@gmail.com>
2024-05-09 22:35:05 -0500

    Improve sequential dashes

--- Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.SequentialDashes.cs
+++ Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.SequentialDashes.cs
@@ -27,12 +27,12 @@ namespace WoTE.Content.NPCs.EoL
         /// <summary>
         /// How long the Empress spends redirecting during her Sequential Dashes attack.
         /// </summary>
-        public static int SequentialDashes_RedirectTime => Utilities.SecondsToFrames(0.23f);
+        public static int SequentialDashes_RedirectTime => Utilities.SecondsToFrames(0.4f);
 
         /// <summary>
         /// How long the Empress spends dashing during her Sequential Dashes attack.
         /// </summary>
-        public static int SequentialDashes_DashTime => Utilities.SecondsToFrames(0.32f);
+        public static int SequentialDashes_DashTime => Utilities.SecondsToFrames(0.25f);
 
         /// <summary>
         /// How long the Empress spends slowing down during her Sequential Dashes attack, after her dash.
@@ -69,12 +69,12 @@ namespace WoTE.Content.NPCs.EoL
             }
             else if (AITimer <= SequentialDashes_RedirectTime + SequentialDashes_DashTime)
             {
-                float dashInterpolant = Utilities.InverseLerp(0f, 10f, AITimer - SequentialDashes_RedirectTime);
+                float dashInterpolant = Utilities.InverseLerp(0f, 6f, AITimer - SequentialDashes_RedirectTime);
                 float targetDirectionErringInterpolant = Utilities.InverseLerp(0f, SequentialDashes_DashTime, AITimer - SequentialDashes_RedirectTime) * Utilities.InverseLerp(200f, 400f, NPC.Distance(Target.Center));
-                Vector2 targetDirectionErring = NPC.SafeDirectionTo(Target.Center) * targetDirectionErringInterpolant * 100f;
-                Vector2 idealVelocity = SequentialDashes_DashDirection.ToRotationVector2() * 125f + targetDirectionErring;
+                Vector2 targetDirectionErring = NPC.SafeDirectionTo(Target.Center) * targetDirectionErringInterpolant * 56f;
+                Vector2 idealVelocity = SequentialDashes_DashDirection.ToRotationVector2() * 100f + targetDirectionErring;
 
-                NPC.velocity = Vector2.Lerp(NPC.velocity, idealVelocity, dashInterpolant * 0.2f);
+                NPC.velocity = Vector2.Lerp(NPC.velocity, idealVelocity, dashInterpolant * 0.6f);
                 NPC.damage = NPC.defDamage;
                 DashAfterimageInterpolant = MathHelper.Lerp(DashAfterimageInterpolant, 1f, 0.3f);
 
@@ -91,7 +91,7 @@ namespace WoTE.Content.NPCs.EoL
             }
             else if (AITimer <= SequentialDashes_RedirectTime + SequentialDashes_DashTime + SequentialDashes_SlowDownTime)
             {
-                NPC.velocity *= 0.81f;
+                NPC.velocity *= 0.7f;
                 DashAfterimageInterpolant *= 0.81f;
             }
             else
@@ -144,7 +144,7 @@ namespace WoTE.Content.NPCs.EoL
             SequentialDashes_DashDirection = NPC.AngleTo(Target.Center);
             NPC.netUpdate = true;
 
-            ScreenShakeSystem.StartShakeAtPoint(NPC.Center, 7f);
+            ScreenShakeSystem.StartShakeAtPoint(NPC.Center, 4f);
         }
     }
 }
