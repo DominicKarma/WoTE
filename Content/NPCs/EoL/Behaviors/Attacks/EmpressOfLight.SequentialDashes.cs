190a86d65917089870aa28b315ed4e2b91095a76
Dominic Karma <farfetchedking541@gmail.com>
2024-05-10 15:53:48 -0500

    Nerf sequential dash speeds

--- Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.SequentialDashes.cs
+++ Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.SequentialDashes.cs
@@ -32,7 +32,7 @@ namespace WoTE.Content.NPCs.EoL
         /// <summary>
         /// How long the Empress spends dashing during her Sequential Dashes attack.
         /// </summary>
-        public static int SequentialDashes_DashTime => Utilities.SecondsToFrames(0.25f);
+        public static int SequentialDashes_DashTime => Utilities.SecondsToFrames(0.34f);
 
         /// <summary>
         /// How long the Empress spends slowing down during her Sequential Dashes attack, after her dash.
@@ -71,8 +71,8 @@ namespace WoTE.Content.NPCs.EoL
             {
                 float dashInterpolant = Utilities.InverseLerp(0f, 6f, AITimer - SequentialDashes_RedirectTime);
                 float targetDirectionErringInterpolant = Utilities.InverseLerp(0f, SequentialDashes_DashTime, AITimer - SequentialDashes_RedirectTime) * Utilities.InverseLerp(200f, 400f, NPC.Distance(Target.Center));
-                Vector2 targetDirectionErring = NPC.SafeDirectionTo(Target.Center) * targetDirectionErringInterpolant * 56f;
-                Vector2 idealVelocity = SequentialDashes_DashDirection.ToRotationVector2() * 100f + targetDirectionErring;
+                Vector2 targetDirectionErring = NPC.SafeDirectionTo(Target.Center) * targetDirectionErringInterpolant * 50f;
+                Vector2 idealVelocity = SequentialDashes_DashDirection.ToRotationVector2() * 76f + targetDirectionErring;
 
                 NPC.velocity = Vector2.Lerp(NPC.velocity, idealVelocity, dashInterpolant * 0.6f);
                 NPC.damage = NPC.defDamage;
