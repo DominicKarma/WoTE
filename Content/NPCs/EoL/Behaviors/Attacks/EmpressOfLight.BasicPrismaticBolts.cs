b6bcad3bf12cf5a8b978b1ffb537987e25cea099
Dominic Karma <farfetchedking541@gmail.com>
2024-05-10 02:04:33 -0500

    Actually maybe that's a bit too much

--- Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.BasicPrismaticBolts.cs
+++ Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.BasicPrismaticBolts.cs
@@ -16,7 +16,7 @@ namespace WoTE.Content.NPCs.EoL
         {
             StateMachine.RegisterTransition(EmpressAIType.BasicPrismaticBolts, null, false, () =>
             {
-                return AITimer >= 210;
+                return AITimer >= 240;
             });
 
             StateMachine.RegisterStateBehavior(EmpressAIType.BasicPrismaticBolts, DoBehavior_BasicPrismaticBolts);
@@ -40,7 +40,7 @@ namespace WoTE.Content.NPCs.EoL
 
         public void DoBehavior_BasicPrismaticBolts_HoverAround()
         {
-            float redirectSpeed = Utils.Remap(AITimer, 0f, 30f, 0.25f, 0.07f);
+            float redirectSpeed = Utils.Remap(AITimer, 0f, 25f, 0.13f, 0.07f);
             Vector2 hoverDestination = Target.Center + new Vector2(MathF.Cos(MathHelper.TwoPi * AITimer / 90f) * 300f, -250f);
             NPC.SmoothFlyNearWithSlowdownRadius(hoverDestination, redirectSpeed, 1f - redirectSpeed, 50f);
             NPC.rotation = NPC.velocity.X * 0.01f;
@@ -49,7 +49,7 @@ namespace WoTE.Content.NPCs.EoL
                 SoundEngine.PlaySound(SoundID.Item164, NPC.Center);
 
             Vector2 handPosition = NPC.Center + new Vector2(30f, -64f).RotatedBy(NPC.rotation);
-            if (Main.netMode != NetmodeID.MultiplayerClient && AITimer % 4 == 3 && AITimer >= 30 && AITimer <= 120 && !NPC.WithinRange(Target.Center, 275f))
+            if (Main.netMode != NetmodeID.MultiplayerClient && AITimer % 4 == 3 && AITimer >= 30 && AITimer <= 120 && !NPC.WithinRange(Target.Center, 150f))
             {
                 Vector2 boltVelocity = (MathHelper.TwoPi * AITimer / 45f).ToRotationVector2() * 10.5f;
                 Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), handPosition, boltVelocity, ModContent.ProjectileType<PrismaticBolt>(), 150, 0f, -1, NPC.target, AITimer / 45f % 1f);
