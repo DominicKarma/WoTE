47989bce4664ed734122e128e5de26910da033e0
Dominic Karma <farfetchedking541@gmail.com>
2024-05-10 19:09:48 -0500

    Move method separation

--- Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.RadialStarBurst.cs
+++ Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.RadialStarBurst.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Luminance.Common.StateMachines;
+﻿using Luminance.Common.StateMachines;
 using Luminance.Common.Utilities;
 using Luminance.Core.Graphics;
 using Microsoft.Xna.Framework;
@@ -66,47 +65,7 @@ namespace WoTE.Content.NPCs.EoL
             int attackRestartDelay = RadialStarBurst_AttackRestartDelay;
             int wrappedTimer = AITimer % (redirectTime + boomDelay + attackRestartDelay);
             if (wrappedTimer <= redirectTime)
-            {
-                bool swapTeleportHappened = false;
-                if (AITimer == 1)
-                {
-                    RadialStarBurst_HorizontalHoverDirection = NPC.OnRightSideOf(Target).ToDirectionInt();
-                    NPC.netUpdate = true;
-                }
-                else if (wrappedTimer == 1)
-                {
-                    RadialStarBurst_HorizontalHoverDirection *= -1f;
-                    swapTeleportHappened = true;
-                }
-
-                if (AITimer <= redirectTime)
-                {
-                    Vector2 teleportDestination = Target.Center - Vector2.UnitY * 150f;
-                    if (AITimer == redirectTime / 2 && !NPC.WithinRange(teleportDestination, 300f))
-                        TeleportTo(teleportDestination);
-
-                    NPC.velocity *= 0.95f;
-                    return;
-                }
-
-                float flySpeedInterpolant = 1f - wrappedTimer / (float)redirectTime;
-                Vector2 hoverDestination = Target.Center + new Vector2(RadialStarBurst_HorizontalHoverDirection * 400f, 100f - Utilities.Convert01To010(wrappedTimer / (float)redirectTime) * 150f);
-                NPC.Center = Vector2.Lerp(NPC.Center, hoverDestination, 0.2f);
-                NPC.velocity += NPC.SafeDirectionTo(hoverDestination) * flySpeedInterpolant * 40f;
-
-                DashAfterimageInterpolant = 1f;
-
-                if (swapTeleportHappened && NPC.OnRightSideOf(Target.Center).ToDirectionInt() == RadialStarBurst_HorizontalHoverDirection)
-                    RadialStarBurst_HorizontalHoverDirection *= -1f;
-
-                if (NPC.velocity.AngleBetween(NPC.SafeDirectionTo(hoverDestination)) >= MathHelper.PiOver2)
-                {
-                    AITimer += redirectTime - wrappedTimer + 1;
-                    NPC.velocity *= 0.25f;
-                    DashAfterimageInterpolant *= 0.4f;
-                    NPC.netUpdate = true;
-                }
-            }
+                DoBehavior_RadialStarBurst_Redirect(wrappedTimer);
             else
             {
                 float idealVerticalSpeed = Utilities.InverseLerpBump(0f, 0.6f, 0.8f, 1f, (wrappedTimer - redirectTime) / (float)boomDelay).Squared() * -20f;
@@ -116,36 +75,7 @@ namespace WoTE.Content.NPCs.EoL
             }
 
             if (wrappedTimer == redirectTime + boomDelay)
-            {
-                SoundEngine.PlaySound(SoundID.Item122);
-                SoundEngine.PlaySound(SoundID.Item160);
-                ScreenShakeSystem.StartShakeAtPoint(NPC.Center, 7.2f);
-                ModContent.GetInstance<DistortionMetaball>().CreateParticle(NPC.Center, Vector2.Zero, 70f, 2f, 0.1f, 0.009f);
-
-                if (Main.netMode != NetmodeID.MultiplayerClient)
-                {
-                    float shootOffsetAngle = NPC.AngleTo(Target.Center);
-                    for (int i = 0; i < 18; i++)
-                    {
-                        float shootAngle = MathHelper.TwoPi * i / 18f + shootOffsetAngle;
-                        Vector2 shootVelocity = shootAngle.ToRotationVector2() * 0.3f;
-
-                        Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), NPC.Center, shootVelocity, ModContent.ProjectileType<StarBolt>(), StarBurstDamage, 0f);
-                    }
-
-                    for (int i = 0; i < 9; i++)
-                    {
-                        Vector2 shootVelocity = (MathHelper.TwoPi * i / 9f).ToRotationVector2() * 6.4f;
-                        Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), NPC.Center, shootVelocity, ModContent.ProjectileType<PrismaticBolt>(), PrismaticBoltDamage, 0f, -1, NPC.target);
-                    }
-
-                    for (int i = 0; i < 7; i++)
-                    {
-                        Vector2 shootVelocity = -NPC.SafeDirectionTo(Target.Center).RotatedByRandom(MathHelper.PiOver2) * Main.rand.NextFloat(1f, 1.7f);
-                        Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), NPC.Center, shootVelocity, ModContent.ProjectileType<PrismaticBolt>(), PrismaticBoltDamage, 0f, -1, NPC.target);
-                    }
-                }
-            }
+                DoBehavior_RadialStarBurst_ShootBurstProjectiles();
 
             if (wrappedTimer >= redirectTime + boomDelay)
             {
@@ -155,26 +85,87 @@ namespace WoTE.Content.NPCs.EoL
         }
 
         /// <summary>
-        /// A polar equation for a star petal with a given amount of points.
+        /// Handles redirecting behaviors during the Empress' Radial Star Burst attack.
         /// </summary>
-        /// <param name="pointCount">The amount of points the star should have.</param>
-        /// <param name="angle">The input angle for the polar equation.</param>
-        public static Vector2 StarPolarEquation(int pointCount, float angle)
+        /// <param name="wrappedTimer">The cyclically wrapped AI timer.</param>
+        public void DoBehavior_RadialStarBurst_Redirect(int wrappedTimer)
         {
-            float spacedAngle = angle;
+            int redirectTime = RadialStarBurst_RedirectTime;
+            bool swapTeleportHappened = false;
 
-            // There should be a star point that looks directly upward. However, that isn't the case for odd star counts with the equation below.
-            // To address this, a -90 degree rotation is performed.
-            if (pointCount % 2 != 0)
-                spacedAngle -= MathHelper.PiOver2;
+            if (AITimer == 1)
+            {
+                RadialStarBurst_HorizontalHoverDirection = NPC.OnRightSideOf(Target).ToDirectionInt();
+                NPC.netUpdate = true;
+            }
+            else if (wrappedTimer == 1)
+            {
+                RadialStarBurst_HorizontalHoverDirection *= -1f;
+                swapTeleportHappened = true;
+            }
 
-            // Refer to desmos to view the resulting shape this creates. It's basically a black box of trig otherwise.
-            float sqrt3 = 1.732051f;
-            float numerator = MathF.Cos(MathHelper.Pi * (pointCount + 1f) / pointCount);
-            float starAdjustedAngle = MathF.Asin(MathF.Cos(pointCount * spacedAngle)) * 2f;
-            float denominator = MathF.Cos((starAdjustedAngle + MathHelper.PiOver2 * pointCount) / (pointCount * 2f));
-            Vector2 result = angle.ToRotationVector2() * numerator / denominator / sqrt3;
-            return result;
+            if (AITimer <= redirectTime)
+            {
+                Vector2 teleportDestination = Target.Center - Vector2.UnitY * 150f;
+                if (AITimer == redirectTime / 2 && !NPC.WithinRange(teleportDestination, 300f))
+                    TeleportTo(teleportDestination);
+
+                NPC.velocity *= 0.95f;
+                return;
+            }
+
+            float flySpeedInterpolant = 1f - wrappedTimer / (float)redirectTime;
+            Vector2 hoverDestination = Target.Center + new Vector2(RadialStarBurst_HorizontalHoverDirection * 400f, 100f - Utilities.Convert01To010(wrappedTimer / (float)redirectTime) * 150f);
+            NPC.Center = Vector2.Lerp(NPC.Center, hoverDestination, 0.2f);
+            NPC.velocity += NPC.SafeDirectionTo(hoverDestination) * flySpeedInterpolant * 40f;
+
+            DashAfterimageInterpolant = 1f;
+
+            if (swapTeleportHappened && NPC.OnRightSideOf(Target.Center).ToDirectionInt() == RadialStarBurst_HorizontalHoverDirection)
+                RadialStarBurst_HorizontalHoverDirection *= -1f;
+
+            if (NPC.velocity.AngleBetween(NPC.SafeDirectionTo(hoverDestination)) >= MathHelper.PiOver2)
+            {
+                AITimer += redirectTime - wrappedTimer + 1;
+                NPC.velocity *= 0.25f;
+                DashAfterimageInterpolant *= 0.4f;
+                NPC.netUpdate = true;
+            }
+        }
+
+        /// <summary>
+        /// Handles burst shot behaviors for the Empress during her Radial Star Bursts attack.
+        /// </summary>
+        public void DoBehavior_RadialStarBurst_ShootBurstProjectiles()
+        {
+            SoundEngine.PlaySound(SoundID.Item122);
+            SoundEngine.PlaySound(SoundID.Item160);
+            ScreenShakeSystem.StartShakeAtPoint(NPC.Center, 7.2f);
+            ModContent.GetInstance<DistortionMetaball>().CreateParticle(NPC.Center, Vector2.Zero, 70f, 2f, 0.1f, 0.009f);
+
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+                return;
+
+            float shootOffsetAngle = NPC.AngleTo(Target.Center);
+            for (int i = 0; i < 18; i++)
+            {
+                float shootAngle = MathHelper.TwoPi * i / 18f + shootOffsetAngle;
+                Vector2 shootVelocity = shootAngle.ToRotationVector2() * 0.3f;
+
+                Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), NPC.Center, shootVelocity, ModContent.ProjectileType<StarBolt>(), StarBurstDamage, 0f);
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                Vector2 shootVelocity = (MathHelper.TwoPi * i / 9f).ToRotationVector2() * 6.4f;
+                Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), NPC.Center, shootVelocity, ModContent.ProjectileType<PrismaticBolt>(), PrismaticBoltDamage, 0f, -1, NPC.target);
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                Vector2 shootVelocity = -NPC.SafeDirectionTo(Target.Center).RotatedByRandom(MathHelper.PiOver2) * Main.rand.NextFloat(1f, 1.7f);
+                Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), NPC.Center, shootVelocity, ModContent.ProjectileType<PrismaticBolt>(), PrismaticBoltDamage, 0f, -1, NPC.target);
+            }
         }
     }
 }
