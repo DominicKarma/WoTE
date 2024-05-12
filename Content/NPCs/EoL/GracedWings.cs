ad8822a0917beb869c226a42684cd52c259b1a2f
Dominic Karma <farfetchedking541@gmail.com>
2024-05-12 04:00:01 -0500

    Add graced wings flap particles

--- Content/NPCs/EoL/GracedWings.cs
+++ Content/NPCs/EoL/GracedWings.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.GameContent.Drawing;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -37,6 +39,32 @@ namespace WoTE.Content.NPCs.EoL
             maxCanAscendMultiplier = MathF.Max(ascentWhenRising, 1.1f);
             maxAscentMultiplier = MathF.Max(maxAscentMultiplier, 2.7f);
             constantAscend = MathF.Max(constantAscend, 0.135f);
+        }
+
+        public override bool WingUpdate(int wings, Player player, bool inUse)
+        {
+            if (!player.HasBuff<GracedWings>() || !inUse)
+                return false;
+
+            if (Main.rand.NextBool())
+            {
+                Dust rainbow = Dust.NewDustPerfect(player.Center + Main.rand.NextVector2Circular(26f, 18f), 261);
+                rainbow.velocity = Vector2.UnitY * Main.rand.NextFloat(2f, 5f);
+                rainbow.color = Main.hslToRgb(Main.rand.NextFloat(0.85f, 1.25f) % 1f, 1f, 0.5f);
+                rainbow.color = Color.Lerp(rainbow.color, Color.HotPink, Main.rand.NextFloat(0.6f));
+                rainbow.noGravity = true;
+            }
+
+            if (Main.rand.NextBool(5))
+            {
+                ParticleOrchestrator.RequestParticleSpawn(true, Main.rand.NextBool() ? ParticleOrchestraType.PrincessWeapon : ParticleOrchestraType.StardustPunch, new ParticleOrchestraSettings
+                {
+                    PositionInWorld = player.Center + Main.rand.NextVector2Circular(26f, 18f),
+                    MovementVector = Main.rand.NextVector2Circular(2f, 4f) + Vector2.UnitY * 5f
+                });
+            }
+
+            return false;
         }
     }
 }
