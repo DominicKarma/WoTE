6a943245643fa72a44186a164d05a1447f3f1d2c
Dominic Karma <farfetchedking541@gmail.com>
2024-05-14 19:34:54 -0500

    Add music pausing

--- Content/NPCs/EoL/SpecificManagers/EmpressSkyScene.cs
+++ Content/NPCs/EoL/SpecificManagers/EmpressSkyScene.cs
@@ -9,13 +9,22 @@ namespace WoTE.Content.NPCs.EoL
 {
     public class EmpressSkyScene : ModSceneEffect
     {
+        /// <summary>
+        /// Whether the game music is paused because the game is paused during the Empress' battle.
+        /// </summary>
+        public static bool MusicIsPaused
+        {
+            get;
+            set;
+        }
+
         public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
 
         public override int Music => EmpressOfLight.Myself?.ModNPC?.Music ?? 0;
 
         public override float GetWeight(Player player) => 0.85f;
 
-        public override bool IsSceneEffectActive(Player player) => NPC.AnyNPCs(ModContent.NPCType<EmpressOfLight>());
+        public override bool IsSceneEffectActive(Player player) => EmpressOfLight.Myself is not null;
 
         public override void Load()
         {
@@ -25,6 +34,24 @@ namespace WoTE.Content.NPCs.EoL
                 SkyManager.Instance[EmpressSky.SkyKey] = new EmpressSky();
                 SkyManager.Instance[EmpressSky.SkyKey].Load();
             }
+
+            On_Main.UpdateAudio += CheckPauseState;
+        }
+
+        private void CheckPauseState(On_Main.orig_UpdateAudio orig, Main self)
+        {
+            bool musicShouldPause = EmpressOfLight.Myself is not null && Main.gamePaused;
+            if (MusicIsPaused != musicShouldPause)
+            {
+                if (musicShouldPause)
+                    Main.audioSystem.PauseAll();
+                else
+                    Main.audioSystem.ResumeAll();
+                MusicIsPaused = musicShouldPause;
+            }
+
+            if (!MusicIsPaused)
+                orig(self);
         }
 
         public override void SpecialVisuals(Player player, bool isActive)
