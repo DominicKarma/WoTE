a036762925ea0e978bb6b39250dd93d4cd6eb120
Dominic Karma <farfetchedking541@gmail.com>
2024-05-17 18:24:08 -0500

    Reduce P2 transition length

--- Content/NPCs/EoL/Behaviors/Animations/EmpressOfLight.Phase2Transition.cs
+++ Content/NPCs/EoL/Behaviors/Animations/EmpressOfLight.Phase2Transition.cs
@@ -34,7 +34,7 @@ namespace WoTE.Content.NPCs.EoL
         /// <summary>
         /// How long the Empress waits during her second phase transition while charging energy.
         /// </summary>
-        public static int Phase2Transition_EnergyChargeUpTime => Utilities.SecondsToFrames(9f);
+        public static int Phase2Transition_EnergyChargeUpTime => Utilities.SecondsToFrames(7.4f);
 
         /// <summary>
         /// How long the Empress spends idle while entering her avatar form during her second phase.
