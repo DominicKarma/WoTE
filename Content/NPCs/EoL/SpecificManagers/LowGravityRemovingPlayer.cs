319ab271bd5d53adb8047e77fbc6c254ed3c21f7
Dominic Karma <farfetchedking541@gmail.com>
2024-05-17 17:54:18 -0500

    Remove low gravity

---
+++ Content/NPCs/EoL/SpecificManagers/LowGravityRemovingPlayer.cs
@@ -0,0 +1,20 @@
+﻿using Luminance.Common.Utilities;
+using Terraria;
+using Terraria.ModLoader;
+using WoTE.Content.NPCs.EoL;
+
+namespace WoTE
+{
+    public class LowGravityRemovingPlayer : ModPlayer
+    {
+        public override void PostUpdate()
+        {
+            float x = (Main.maxTilesX / 4200f).Squared();
+            float spaceGravityMult = (float)((Player.position.Y / 16f - (60f + 10f * x)) / (Main.worldSurface / 6f));
+            bool inSpace = spaceGravityMult < 1f; ;
+
+            if (EmpressOfLight.Myself is not null && inSpace)
+                Player.gravity = Player.defaultGravity;
+        }
+    }
+}
