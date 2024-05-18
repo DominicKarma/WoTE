c4fba85ccb47a93548418c2b3d4036f25e3957b5
Dominic Karma <farfetchedking541@gmail.com>
2024-05-18 10:39:32 -0500

    Reduce Terraprisma count

--- Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.ConvergingTerraprismas.cs
+++ Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.ConvergingTerraprismas.cs
@@ -34,7 +34,7 @@ namespace WoTE.Content.NPCs.EoL
         /// <summary>
         /// The amount of Terraprisma instances the Empress summons for her Converging Terraprismas attack.
         /// </summary>
-        public int ConvergingTerraprismas_TerraprismaCount => 8;
+        public static int ConvergingTerraprismas_TerraprismaCount => 7;
 
         /// <summary>
         /// How long Terraprismas summoned by the Empress take to fade in.
