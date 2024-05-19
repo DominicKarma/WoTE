3654fc8722393407ff8f7b9a536aa4cefd19990b
Dominic Karma <farfetchedking541@gmail.com>
2024-05-18 21:21:38 -0500

    Tweaks to prismatic bolt spin shoot conditions

--- Content/NPCs/EoL/Behaviors/Attacks/Phase2/EmpressOfLight.PrismaticBoltSpin.cs
+++ Content/NPCs/EoL/Behaviors/Attacks/Phase2/EmpressOfLight.PrismaticBoltSpin.cs
@@ -88,7 +88,8 @@ namespace WoTE.Content.NPCs.EoL
             float spinSpeed = Utilities.InverseLerp(0f, 40f, AITimer) * MathHelper.TwoPi / 36f;
             PrismaticBoltSpin_SpinAngle += spinSpeed * PrismaticBoltSpin_SpinDirection;
 
-            if (Main.netMode != NetmodeID.MultiplayerClient && AITimer % 5 == 4 && AITimer >= 36)
+            bool canFire = AITimer >= 36 && Target.velocity.AngleBetween(Target.SafeDirectionTo(NPC.Center)) >= MathHelper.Pi / 11.5f;
+            if (Main.netMode != NetmodeID.MultiplayerClient && AITimer % 4 == 3 && canFire)
             {
                 Vector2 boltVelocity = NPC.velocity.SafeNormalize(Vector2.UnitY) * 10f;
                 Utilities.NewProjectileBetter(NPC.GetSource_FromAI(), NPC.Center, boltVelocity, ModContent.ProjectileType<PrismaticBolt>(), PrismaticBoltDamage, 0f, -1, NPC.target, AITimer / 45f % 1f);
