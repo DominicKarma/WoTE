468cf444eec3f38d2ef02eb5e858e6489e31ec46
Dominic Karma <farfetchedking541@gmail.com>
2024-05-20 18:31:35 -0500

    Reduce converging moonlight positions

--- Content/NPCs/EoL/Projectiles/ConvergingMoonlight.cs
+++ Content/NPCs/EoL/Projectiles/ConvergingMoonlight.cs
@@ -38,7 +38,7 @@ namespace WoTE.Content.NPCs.EoL.Projectiles
         public override void SetStaticDefaults()
         {
             ProjectileID.Sets.TrailingMode[Type] = 2;
-            ProjectileID.Sets.TrailCacheLength[Type] = 96;
+            ProjectileID.Sets.TrailCacheLength[Type] = 40;
             ProjectileID.Sets.DrawScreenCheckFluff[Type] = 2000;
         }
 
@@ -52,7 +52,7 @@ namespace WoTE.Content.NPCs.EoL.Projectiles
             Projectile.hostile = true;
             Projectile.hide = true;
             Projectile.timeLeft = Lifetime + 45;
-            RelatveOldPositions = new Vector2[Lifetime];
+            RelatveOldPositions = new Vector2[40];
 
             CooldownSlot = ImmunityCooldownID.Bosses;
         }
