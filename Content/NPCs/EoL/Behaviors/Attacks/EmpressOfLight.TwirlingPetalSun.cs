ec070aa8856ca15af9b607955c204c88f6d05664
Dominic Karma <farfetchedking541@gmail.com>
2024-05-20 09:22:12 -0500

    Twirling Petal Sun daytime buff

--- Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.TwirlingPetalSun.cs
+++ Content/NPCs/EoL/Behaviors/Attacks/EmpressOfLight.TwirlingPetalSun.cs
@@ -39,7 +39,7 @@ namespace WoTE.Content.NPCs.EoL
         /// <summary>
         /// The amount of time the Empress should wait after the twirling petals burst to choose a new attack.
         /// </summary>
-        public int TwirlingPetalSun_AttackTransitionDelay => Utilities.SecondsToFrames(ByPhase(1.85f, 1f));
+        public int TwirlingPetalSun_AttackTransitionDelay => Utilities.SecondsToFrames(ByPhase(1.76f, 1f) - Main.dayTime.ToInt() * 0.35f);
 
         /// <summary>
         /// The amount of petals the Empress summons during the Twirling Petal Sun attack.
