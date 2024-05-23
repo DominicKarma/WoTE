d8e98ebfc4def892793bd1247ad728f4fce71424
Dominic Karma <farfetchedking541@gmail.com>
2024-05-23 08:36:15 -0500

    Increase lacewing defense again

--- Content/NPCs/EoL/Lacewing.cs
+++ Content/NPCs/EoL/Lacewing.cs
@@ -154,7 +154,7 @@ namespace WoTE.Content.NPCs.EoL
             NPC.damage = 110;
             NPC.width = 20;
             NPC.height = 20;
-            NPC.defense = 92;
+            NPC.defense = 125;
             NPC.SetLifeMaxByMode(200000, 300000, 400000);
 
             if (Main.expertMode)
